f05c2be623a5e9a5d9fe6c3f33c7e3e4baf98cba
Hugh Cole-Baker <sigmaris@gmail.com>
2019-05-12 15:22:35 +0100

    Try building latest mesa

--- installtarget.sh
+++ installtarget.sh
@@ -1,2 +1,15 @@
 apt-get update
-apt-get install -y openssh-server 
+apt-get install -y build-essential dbus dbus-user-session git make openssh-server vim
+systemctl enable systemd-resolved
+systemctl enable systemd-networkd
+
+# Build latest mesa debian package:
+apt-get install libclc-dev/experimental
+apt-get build-dep mesa
+mkdir -p /opt/build
+pushd /opt/build
+git clone --branch debian-experimental https://salsa.debian.org/xorg-team/lib/mesa.git
+pushd mesa
+
+popd
+popd
--- macos-img-build/run_host_image_macos.sh
+++ macos-img-build/run_host_image_macos.sh
@@ -6,7 +6,7 @@ qemu-system-x86_64 \
 	-hda debian.qcow \
 	-netdev user,id=net0,net=10.0.2.0/24,hostname=rp64builder,domainname=localdomain,hostfwd=tcp:127.0.0.1:10022-:22 \
 	-device e1000,netdev=net0,mac=52:54:98:76:54:32 \
-	-m 2048 \
+	-m 4096 \
 	-nographic \
 	-serial mon:telnet::10023,server,nowait &
 
