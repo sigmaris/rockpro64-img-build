23939cceec7ca577a8257a1ab72f31295e2dcc6f
Hugh Cole-Baker <sigmaris@gmail.com>
2019-09-17 23:27:44 +0100

    Add user to render group so they can use Panfrost

--- scripts/setup-user.sh
+++ scripts/setup-user.sh
@@ -4,7 +4,7 @@ adduser --gecos "Rock User" \
   --shell /bin/bash \
   rock
 
-usermod -a -G adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,netdev rock
+usermod -a -G adm,dialout,cdrom,sudo,audio,video,render,plugdev,games,users,input,netdev rock
 
 echo "rock:rock.64" | chpasswd
 passwd -e rock
