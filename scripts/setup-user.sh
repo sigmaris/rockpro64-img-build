a50dda296ee853e08d5f305ea087548277ea457c
Hugh Cole-Baker <sigmaris@gmail.com>
2020-06-29 18:36:49 +0100

    Support for building Ubuntu images as well as Debian

--- scripts/setup-user.sh
+++ scripts/setup-user.sh
@@ -1,10 +1,19 @@
-#!/bin/sh
+#!/bin/sh -e
 adduser --gecos "Rock User" \
   --disabled-password \
   --shell /bin/bash \
   rock
 
-usermod -a -G adm,dialout,cdrom,sudo,audio,video,render,plugdev,games,users,input,netdev rock
+usermod -a -G adm,dialout,cdrom,sudo,audio,video,render,plugdev,games,users,input rock
 
+# Ubuntu doesn't have the netdev group
+if getent group netdev >/dev/null
+then
+	usermod -a -G netdev rock
+fi
+
+# Set initial password for rock
 echo "rock:rock.64" | chpasswd
+
+# Expire password so it requires changing immediately after login
 passwd -e rock
